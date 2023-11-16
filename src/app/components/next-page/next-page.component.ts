1c951bfaacda3aa5316762bc8e641b03e7232d38
Alvin Tan <alvin.tan1912@gmail.com>
2023-11-16 16:07:49 +0800

    done next-page component

--- src/app/components/next-page/next-page.component.ts
+++ src/app/components/next-page/next-page.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-next-page',
   templateUrl: './next-page.component.html',
-  styleUrls: ['./next-page.component.css']
+  styleUrls: ['./next-page.component.css'],
 })
 export class NextPageComponent {
+  @Input() title = '';
 
+  public onClick(): void {
+    console.log('scroll to next page');
+  }
 }
