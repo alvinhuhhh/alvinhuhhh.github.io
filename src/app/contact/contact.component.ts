73a11c5641cfa32f79d15ac5bd973c2aa97cf95a
Alvin Tan <alvin.tan1912@gmail.com>
2023-12-01 15:31:18 +0800

    layout for contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
-
+  email: string = 'alvin.tan1912@gmail.com';
+  github: string = 'github.com/alvinhuhhh';
+  linkedin: string = 'linkedin.com/in/atwb96';
 }
