3efd8eee45d6da218cbbfef274620dc0310dd5af
Alvin Tan <alvin.tan1912@gmail.com>
2023-12-15 20:09:20 +0800

    add clickable links to contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,4 +9,7 @@ export class ContactComponent {
   email: string = 'alvin.tan1912@gmail.com';
   github: string = 'github.com/alvinhuhhh';
   linkedin: string = 'linkedin.com/in/atwb96';
+
+  github_url: string = 'https://github.com/alvinhuhhh';
+  linkedin_url: string = 'https://www.linkedin.com/in/alvintan96';
 }
